5eda93252784f3ea89534c71be5b05fcf0d32c83
Todd <todd@blockpane.com>
2021-07-25 12:04:23 -0600

    adding % of consensus power to chart

--- summary.go
+++ summary.go
@@ -10,12 +10,14 @@ import (
 )
 
 type Summary struct {
-	BlockNum   int               `json:"block_num"`
-	Timestamp  int64             `json:"timestamp"`
-	DeltaSec   float64           `json:"delta_sec"`
-	Missed     int               `json:"missed"`
-	Validators map[string]string `json:"missing"`
-	Proposer   string            `json:"proposer"`
+	BlockNum    int               `json:"block_num"`
+	Timestamp   int64             `json:"timestamp"`
+	DeltaSec    float64           `json:"delta_sec"`
+	Missed      int               `json:"missed"`
+	Validators  map[string]string `json:"missing"`
+	Proposer    string            `json:"proposer"`
+	VotePower   uint64            `json:"vote_power"`
+	VoteMissing uint64            `json:"vote_missing"`
 }
 
 func summarize(blocknum int, ts int64, proposer string, signers []string, addrs map[string]bool, valcons map[string]string, validators []Validator) *Summary {
@@ -26,8 +28,11 @@ func summarize(blocknum int, ts int64, proposer string, signers []string, addrs 
 		Validators: make(map[string]string),
 	}
 	names := make(map[string]string)
+	powers := make(map[string]uint64)
 	for _, v := range validators {
 		names[v.ValidatorAddress] = v.Description.Moniker
+		powers[v.ValidatorAddress] = v.Tokens
+		s.VotePower += v.Tokens
 	}
 	s.Proposer = names[proposer]
 	for i := range signers {
@@ -36,6 +41,7 @@ func summarize(blocknum int, ts int64, proposer string, signers []string, addrs 
 	for k, v := range addrs {
 		if !v {
 			s.Validators[names[k]] = valcons[k]
+			s.VoteMissing += powers[k]
 		}
 	}
 	return &s
@@ -118,26 +124,30 @@ func TopMissed(summaries []*Summary, blocks int, prefix, cosmosApi string) ([]*T
 // BlockChart is just another way of presenting the same data that is easier to use with some charting libraries, where
 // each series is presented as an array
 type BlockChart struct {
-	Blocks []int     `json:"blocks"` // block number
-	Time   []string  `json:"time"`   // unix time in ms
-	Missed []int     `json:"missed"` // number of missing validators
-	Took   []float64 `json:"took"`   // time since last block in seconds
+	Blocks  []int     `json:"blocks"`          // block number
+	Time    []string  `json:"time"`            // time as a string
+	Missed  []int     `json:"missed"`          // number of missing validators
+	MissPct []float64 `json:"missing_percent"` // vote power missing (shows actual risk of losing consensus.)
+	Took    []float64 `json:"took"`            // time since last block in seconds
 }
 
 func SummariesToChart(s []*Summary) ([]byte, error) {
 	bc := BlockChart{
-		Blocks: make([]int, len(s)),
-		Time:   make([]string, len(s)),
-		Missed: make([]int, len(s)),
-		Took:   make([]float64, len(s)),
+		Blocks:  make([]int, len(s)),
+		Time:    make([]string, len(s)),
+		Missed:  make([]int, len(s)),
+		Took:    make([]float64, len(s)),
+		MissPct: make([]float64, len(s)),
 	}
 	for i := range s {
 
 		bc.Blocks[i] = s[i].BlockNum
-		bc.Time[i] = time.Unix(s[i].Timestamp / 1000, 0).UTC().Format(time.Stamp)
+		bc.Time[i] = time.Unix(s[i].Timestamp/1000, 0).UTC().Format(time.Stamp)
 		bc.Missed[i] = s[i].Missed
 		bc.Took[i] = s[i].DeltaSec
+		if s[i].VotePower > 0 {
+			bc.MissPct[i] = (float64(s[i].VoteMissing) / float64(s[i].VotePower)) * 100.0
+		}
 	}
 	return json.MarshalIndent(bc, "", "  ")
 }
-
