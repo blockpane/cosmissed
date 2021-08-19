a40474123910add13b86206307cf5e50734de5fa
Todd <todd@blockpane.com>
2021-08-18 21:32:54 -0600

    try to fix disappearing ISP stats

--- net-info.go
+++ net-info.go
@@ -238,6 +238,8 @@ type NodeLocation struct {
 
 var nodeLocCache = make(map[string]*NodeLocation)
 
+var ispList = make(map[string]map[string]map[string]int)
+
 func NetworkSummary(d *Discovered, p PeerMap) NetworkStats {
 	if d == nil {
 		return NetworkStats{}
@@ -263,7 +265,6 @@ func NetworkSummary(d *Discovered, p PeerMap) NetworkStats {
 	}
 	cityFound := make(map[string]int)
 	countryFound := make(map[string]int)
-	ispList := make(map[string]map[string]map[string]int)
 	increment := func(s string, w map[string]int) {
 		w[s] += 1
 	}
